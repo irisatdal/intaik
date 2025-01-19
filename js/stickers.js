2dde14fa078c0f9067317a33b98e60975bb7c366
jennamakin <jn221861@dal.ca>
2025-01-19 14:45:19 -0400

    refactered things

--- js/stickers.js
+++ js/stickers.js
@@ -1,23 +1,33 @@
 const go_btn = document.getElementById("go-button");
-const sticker_pkg =document.getElementById("sticker-pkg");
+
+let legendary = ["profile"]
+let rare = ["profile"]
+let common = ["profile"]
 
 
 go_btn.addEventListener('click', open_pkg);
 
+
 function open_pkg() {
-    let random = Math.random()
-
+    document.getElementById("sticker-pkg") .src="images/open-sticker.PNG";
+    let random = Math.random();
+    let sticker;
     if (random <= 0.1){
-
+        sticker = legendary.shift();
     }
-    
-    else if (random <= 0.3){
 
+    else if (random <= 0.3){
+        sticker = rare.shift();
     }
 
     else{
-
+        sticker = common.shift();
     }
-    
-    window.open(indexedDB.html);
+
+    setTimeout(show_sticker, 800)
+
+}
+
+function show_sticker(sticker){
+    document.getElementById("sticker-pkg") .src= sticker + "PNG";
 }
